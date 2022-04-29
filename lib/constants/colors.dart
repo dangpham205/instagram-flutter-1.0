731e4bd573b76fb446726c0e3873a818c35eeada
Dawnny <haidawng205@gmail.com>
2022-04-29 08:58:01 +0700

    push

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const mobileBackgroundColor = Colors.white;
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
 const blueColor = Color.fromRGBO(0, 149, 246, 1);
-const primaryColor = Colors.white;
-const secondaryColor = Colors.grey;
+const primaryColor = Colors.black;
+const secondaryColor = Colors.black;
 const loginButton = Color.fromARGB(255, 110, 87, 240);
-const darkColor = Color.fromARGB(255, 19, 19, 19);
+const darkColor = Color.fromARGB(255, 227, 227, 227);    //seperate color
